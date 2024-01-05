0a39f6703e30e06ecbde38af78240ae28c713fb6
clintonium-119 <clint.beacock@gmail.com>
2024-01-05 14:24:29 -0500

    Allow overriding default well styles

--- app/partials/Well.tsx
+++ app/partials/Well.tsx
@@ -2,12 +2,15 @@ import { ReactElement, ReactNode } from 'react';
 
 interface WellProps {
   children: ReactNode | ReactElement;
+  className?: string;
 }
 
-export default function Well({ children }: WellProps): ReactElement {
-  return (
-    <div className="flex flex-col p-6 lg:p-8 bg-content1 text-content1-foreground rounded-xl gap-4 border-1 border-content2">
-      {children}
-    </div>
-  );
+export default function Well({
+  children,
+  className = '',
+}: WellProps): ReactElement {
+  const defaultClasses =
+    'flex flex-col p-6 lg:p-8 bg-content1 text-content1-foreground rounded-xl gap-4 border-1 border-content2';
+
+  return <div className={`${defaultClasses} ${className}`}>{children}</div>;
 }
