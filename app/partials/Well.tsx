bfa26698187c9148383183c7001c735b4b93adbe
clintonium-119 <clint.beacock@gmail.com>
2024-01-12 16:31:21 -0500

    Well styling

--- app/partials/Well.tsx
+++ app/partials/Well.tsx
@@ -10,7 +10,7 @@ export default function Well({
   className = '',
 }: WellProps): ReactElement {
   const defaultClasses =
-    'flex flex-col p-6 lg:p-8 bg-content1 text-content1-foreground rounded-xl gap-4 border-1 border-content2';
+    'flex flex-grow flex-col p-6 lg:p-8 bg-content1 text-content1-foreground rounded-xl gap-4 border-1 border-content2';
 
   return <div className={`${defaultClasses} ${className}`}>{children}</div>;
 }
