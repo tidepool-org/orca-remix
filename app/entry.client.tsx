c89ae508f9ad7484514c482794ca203ec66b9f5a
clintonium-119 <clint.beacock@gmail.com>
2025-11-26 16:32:47 -0500

    Fix hydration issues caused by requestly

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,6 +2,10 @@ import { HydratedRouter } from "react-router/dom";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
+// Remove Requestly injected elements before hydration - they cause hydration mismatches
+const requestlyElements = document.querySelectorAll('rq-implicit-test-rule-widget, rq-overlay');
+requestlyElements.forEach(el => el.remove());
+
 startTransition(() => {
   hydrateRoot(
     document,
