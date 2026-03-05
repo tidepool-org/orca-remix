ebe02325b964692c6a27d4dc48e2908237e63bd8
clintonium-119 <clint.beacock@gmail.com>
2026-03-05 16:02:24 -0500

    fix: sidebar not expanding/collapsing properly on largest width viewport

--- app/partials/Sidebar.tsx
+++ app/partials/Sidebar.tsx
@@ -117,7 +117,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarOpenProps) {
       <div
         id="sidebar"
         ref={sidebar}
-        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-24 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-content4 text-content4-foreground p-4 pt-0 transition-all duration-200 ease-in-out ${
+        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-24 lg:sidebar-expanded:!w-64 shrink-0 bg-content4 text-content4-foreground p-4 pt-0 transition-all duration-200 ease-in-out ${
           sidebarOpen ? 'translate-x-0' : '-translate-x-64'
         }`}
       >
@@ -139,14 +139,14 @@ function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarOpenProps) {
 
           <NavLink to="/">
             <Logo
-              className={`hidden lg:block 2xl:hidden right-${
+              className={`hidden lg:block right-${
                 sidebarExpanded ? 3 : 0
               }`}
               src={sidebarExpanded ? undefined : SmallLogo}
               width={sidebarExpanded ? undefined : 32}
               theme={Theme.DARK}
             />
-            <Logo className="lg:hidden 2xl:block right-2" theme={Theme.DARK} />
+            <Logo className="lg:hidden right-2" theme={Theme.DARK} />
           </NavLink>
         </div>
 
@@ -172,7 +172,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarOpenProps) {
         </nav>
 
         {/* Expand / collapse button */}
-        <div className="pt-3 hidden lg:inline-flex 2xl:hidden justify-center sidebar-expanded:justify-end mt-auto">
+        <div className="pt-3 hidden lg:inline-flex justify-center sidebar-expanded:justify-end mt-auto">
           <div className="px-2 py-2">
             <Button
               className="bg-transparent text-content4-foreground"
